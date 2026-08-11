c8b7f54f7dc5a71917a0ac1171ae6f7faa9ab132
whites <2863692760@qq.com>
2026-08-11 16:20:45 +0800

    fix: full power when actual temp above target, PID only when reached

--- sourceCode/MDK_Hardware/Src/bsp_pid.c
+++ sourceCode/MDK_Hardware/Src/bsp_pid.c
@@ -113,8 +113,8 @@ void PID_Update(void)
         return;
     }
 
-    /* Full power cooling when actual temp >= 0C */
-    if (g_cooling_pid.current_temp >= 0)
+    /* Full power cooling when actual temp above target */
+    if (g_cooling_pid.current_temp > g_cooling_pid.target_temp)
     {
         g_cooling_pid.output = PID_OUTPUT_MAX;
         g_cooling_pid.integral = 0;
