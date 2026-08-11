bf2ea5335b40d2d9cd8dfa7c66baade286e0417b
whites <2863692760@qq.com>
2026-08-11 16:32:39 +0800

    fix: full power when target < 0C, PID completely bypassed

--- sourceCode/MDK_Hardware/Src/bsp_pid.c
+++ sourceCode/MDK_Hardware/Src/bsp_pid.c
@@ -113,6 +113,15 @@ void PID_Update(void)
         return;
     }
 
+    /* Full power when target below 0C, PID not used */
+    if (g_cooling_pid.target_temp < 0)
+    {
+        g_cooling_pid.output = PID_OUTPUT_MAX;
+        g_cooling_pid.integral = 0;
+        g_cooling_pid.prev_error = 0;
+        return;
+    }
+
     /* Full power cooling when actual temp above target */
     if (g_cooling_pid.current_temp > g_cooling_pid.target_temp)
     {
